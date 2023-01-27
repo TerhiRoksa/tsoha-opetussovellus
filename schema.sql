fa7d6723c6ada22247eefc898b177cc979d3c24c
Terhi Röksä <terhi.roksa@elisanet.fi>
2023-01-27 12:18:48 +0200

    updateschema.sql

--- schema.sql
+++ schema.sql
@@ -2,5 +2,21 @@ CREATE TABLE Users (
 	id SERIAL PRIMARY KEY, 
 	username TEXT UNIQUE, 
 	password TEXT, 	
-	usertype INTEGER);
+	usertype INTEGER
+);
 
+CREATE TABLE polls (
+    id SERIAL PRIMARY KEY,
+    topic TEXT
+);
+
+CREATE TABLE choices (
+    id SERIAL PRIMARY KEY,
+    poll_id INTEGER REFERENCES polls,
+    choice TEXT
+);
+
+CREATE TABLE answers (
+    id SERIAL PRIMARY KEY,
+    choice_id INTEGER REFERENCES choices
+);
