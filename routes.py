f061d96fb555f2e9930a28b158ce91061a977ca4
Terhi Röksä <terhi.roksa@elisanet.fi>
2023-01-31 12:59:56 +0200

    refactoring login

---
+++ routes.py
@@ -0,0 +1,99 @@
+from app import app
+from flask import render_template, request, redirect, session
+from db import db
+import users
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]  
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/polls")
+def polls():
+    sql = "SELECT id, topic FROM polls ORDER BY id DESC"
+    result = db.session.execute(sql)
+    polls = result.fetchall()
+    return render_template("polls.html", polls=polls)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    topic = request.form["topic"]
+    sql = "INSERT INTO polls (topic) VALUES (:topic) RETURNING id"
+    result = db.session.execute(sql, {"topic":topic})
+    poll_id = result.fetchone()[0]
+    choices = request.form.getlist("choice")
+    for choice in choices:
+        if choice != "":
+            sql = "INSERT INTO choices (poll_id, choice) VALUES (:poll_id, :choice)"
+            db.session.execute(sql, {"poll_id":poll_id, "choice":choice})
+    db.session.commit()
+    return redirect("/polls")
+
+@app.route("/poll/<int:id>")
+def poll(id):
+    sql = "SELECT topic FROM polls WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic = result.fetchone()[0]
+    sql = "SELECT id, choice FROM choices WHERE poll_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    choices = result.fetchall()
+    return render_template("poll.html", id=id, topic=topic, choices=choices)
+
+@app.route("/answer", methods=["POST"])
+def answer():
+    poll_id = request.form["id"]
+    if "answer" in request.form:  
+        choice_id = request.form["answer"]  
+        sql = "INSERT INTO answers (choice_id) VALUES (:choice_id)"
+        db.session.execute(sql, {"choice_id":choice_id})
+        db.session.commit()
+    return redirect("/result/" + str(poll_id))
+    
+@app.route("/result/<int:id>")
+def result(id):
+    sql = "SELECT topic FROM polls WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic = result.fetchone()[0]
+    sql = "SELECT c.choice, COUNT(a.id) FROM choices c LEFT JOIN answers a " \
+          "ON c.id=a.choice_id WHERE c.poll_id=:poll_id GROUP BY c.id"
+    result = db.session.execute(sql, {"poll_id":id})
+    choices = result.fetchall()
+    return render_template("result.html", topic=topic, choices=choices)
+
